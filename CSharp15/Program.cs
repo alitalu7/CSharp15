46bcec6f68e1e0007b10c38b5cded9b2c85c63d3
alitalu7 <55026802+alitalu7@users.noreply.github.com>
2022-06-25 17:22:16 +0300

    Add project files.

---
+++ CSharp15/Program.cs
@@ -0,0 +1,7 @@
+﻿DateTime gun = DateTime.Now;
+
+Console.WriteLine("İçinde bulunduğumuz yıl " + gun.Year);
+Console.WriteLine("İçinde bulunduğumuz ay " + gun.Month);
+Console.WriteLine("İçinde bulunduğumuz gün " + gun.Day);
+Console.WriteLine("Şuan yılın " + gun.DayOfYear + ". günü");
+Console.WriteLine("Şuan saat : "+ gun.Hour + ":" + gun.Minute);
